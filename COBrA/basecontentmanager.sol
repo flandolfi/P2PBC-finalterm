687425f2f48a5b40637e01f6f76a64f3cf6ac6f7
Francesco Landolfi <fran.landolfi@gmail.com>
2018-06-14 11:05:59 +0200

    Add NatSpec comments

--- COBrA/basecontentmanager.sol
+++ COBrA/basecontentmanager.sol
@@ -1,10 +1,14 @@
 pragma solidity ^0.4.20;
 
+/// @title `BaseContentManager`: the Content Manager Interface
+/// @author Francesco Landolfi
+/// @notice This interface provides the basic set of functions that a content
+///         manager should provide to interact with the `Catalog` contract
 contract BaseContentManager {
     event AccessGranted(address to, uint until);
     event ContentConsumed(address by);
 
-    function getInfo() external view returns (address, string, uint);
+    function getInfo() external view returns (address author, string title, uint genre);
     function grantAccess(address _account, uint _until) external;
     function consumeContent() external;
 }
